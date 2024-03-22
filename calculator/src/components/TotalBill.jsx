b81df526ad06744c8e3de197453c31105d5ee61a
JeffDanielle <bayotjeff@gmail.com>
2024-03-22 18:06:51 +0800

    Fixed computation bug

--- calculator/src/components/TotalBill.jsx
+++ calculator/src/components/TotalBill.jsx
@@ -7,7 +7,8 @@ const TotalBill = () => {
     const [Tip, setTip] = useState(0);
     const [friendTip, setFriendTip] = useState(0);
 
-    const TotalTip = (((Tip / 100) + (friendTip / 100)) / 2) * 100
+    const TotalTip = ((((Tip / 100) + (friendTip / 100)) / 2) * BillNumber)
+    const roundedTotalTip = parseFloat(TotalTip.toFixed(1))
     const TotalFee = TotalTip + BillNumber
 
     const handleResetBtn = () => {
@@ -20,7 +21,7 @@ const TotalBill = () => {
             <Bill BillNumber={BillNumber} changeBill={(e) => setBillNumber(+e.target.value)} />
             <TipInput Tip={Tip} changeTip={(e) => setTip(+e.target.value)}>How did you like the service?</TipInput>
             <TipInput Tip={friendTip} changeTip={(e) => setFriendTip(+e.target.value)}>How did your friend like the service?</TipInput>
-            {BillNumber ? <h1 className='text-2xl font-bold mx-2 pt-2'>You pay ${TotalFee}(${BillNumber} + ${TotalTip} tip)</h1> : null}
+            {BillNumber ? <h1 className='text-2xl font-bold mx-2 pt-2'>You pay ${TotalFee}(${BillNumber} + ${roundedTotalTip} tip)</h1> : null}
             {BillNumber ? <ResetInput handleReset={handleResetBtn}>Reset</ResetInput> : null}
         </div>
     );
